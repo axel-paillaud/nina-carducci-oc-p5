ce2374f4ab843c5efd25920953cae6ac9eb3c126
Axel Paillaud <a.paillaud75@gmail.com>
2023-03-06 09:41:18 +0100

    fix bug on the right button on jquery gallery

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
